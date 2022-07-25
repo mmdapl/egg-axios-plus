3dc021a682a8e6576b248e21487cb5126cfa19f1
fairy爱哭又可爱 <fariy_vip@2925.com>
2022-07-25 11:40:57 +0800

    feat: 更新example代码

--- example/app/router.js
+++ example/app/router.js
@@ -1,11 +1,10 @@
 'use strict';
 
 /**
- * @param {Egg.Application} app - egg application
+ * 接口路由
  */
 module.exports = app => {
   const { router, controller } = app;
   router.all('/index', controller.home.index);
-
   router.get('/test', controller.home.test);
 };
