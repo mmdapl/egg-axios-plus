e83521534ab697e41f3219855c766527e52e23e3
fairy爱哭又可爱 <fariy_vip@2925.com>
2022-07-25 11:29:06 +0800

    feat: update example code

--- example/config/plugin.js
+++ example/config/plugin.js
@@ -1,11 +1,5 @@
 'use strict';
-
-/** @type Egg.EggPlugin */
 module.exports = {
-  // had enabled by egg
-  // static: {
-  //   enable: true,
-  // }
   axiosPlus: {
     enable: true,
     package: 'egg-axios-plus',
